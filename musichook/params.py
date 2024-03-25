05dd18a333cbf3604c7ccf102cc3479fac9aca2a
Your Name <youremail@example.com>
2024-03-25 11:40:52 +0100

    Clean Jupyter demo audio similarity

--- musichook/params.py
+++ musichook/params.py
@@ -6,6 +6,7 @@ PATHS = {
     "ROOT_PROJECT_PATH": ROOT_PROJECT_PATH,
     "SPOTIFY_FOLDER": os.path.join(ROOT_PROJECT_PATH, "data/spotify"),
     "MUSIC_FOLDER": os.path.join(ROOT_PROJECT_PATH, "data/music"),
+    "STAGING_FOLDER": os.path.join(ROOT_PROJECT_PATH, "data/staging"),
 }
 
 SECRETS = {
